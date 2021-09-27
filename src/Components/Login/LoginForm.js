7691750b9817c20618ac1169ecf3ed7336632b5f
Gabriel Souza <gabrielss_@outlook.com>
2021-09-27 12:17:37 -0300

    login routes, componentes input e button

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../Forms/Button";
+import Input from "../Forms/Input";
+
+const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await fetch(
+      "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }
+    );
+    const json = await response.json();
+    console.log(response);
+    console.log(json);
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
