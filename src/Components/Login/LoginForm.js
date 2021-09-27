cab36a44f5e1b286f46c808223fff268dbdd5dcb
Gabriel Souza <gabrielss_@outlook.com>
2021-09-27 16:37:06 -0300

    useForm Hook e validacao

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,38 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../Forms/Button";
 import Input from "../Forms/Input";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const response = await fetch(
-      "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-      }
-    );
-    const json = await response.json();
-    console.log(response);
-    console.log(json);
+    if (username.validate() && password.validate()) {
+      const response = await fetch(
+        "https://dogsapi.origamid.dev/json/jwt-auth/v1/token",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(),
+        }
+      );
+      const json = await response.json();
+      console.log(response);
+      console.log(json);
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input
+          label="Usuário"
+          type="text"
+          name="username"
+          value={username.value}
+          onChange={username.onChange}
+          error={username.error}
+          onBlur={username.onBlur}
+        />
+        <Input
+          label="Senha"
+          type="password"
+          name="password"
+          value={password.value}
+          onChange={password.onChange}
+          error={password.error}
+          onBlur={password.onBlur}
+        />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
---
+++ src/Hooks/useForm.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const validationTypes = {
+  email: {
+    regex:
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: "Preencha um email válido",
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = React.useState("");
+  const [error, setError] = React.useState(null);
+
+  function validate(value) {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError("Preencha um valor");
+      return false;
+    } else if (
+      validationTypes[type] &&
+      !validationTypes[type].regex.test(value)
+    ) {
+      setError(validationTypes[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  }
+
+  function onChange(event) {
+    setValue(event.target.value);
+    if (error) {
+      validate(event.target.value);
+    }
+  }
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
