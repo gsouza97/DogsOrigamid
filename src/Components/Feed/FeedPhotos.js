7013c32179bf41c3a1857d56771d798df2db1e39
Gabriel Souza <gabrielss_@outlook.com>
2021-09-30 16:45:32 -0300

    fotos no feed

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./FeedPhotos.module.css";
+import FeedPhotosItem from "./FeedPhotosItem";
+import useFetch from "../../Hooks/useFetch";
+import { PHOTOS_GET } from "../../api";
+import Error from "../Helper/Error";
+import Loading from "../Helper/Loading";
+
+const FeedPhotos = () => {
+  const fetchHook = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const api = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const response = await (
+        await fetchHook.request(api.url, api.options)
+      ).json;
+      console.log(response);
+    }
+    fetchPhotos();
+  }, [fetchHook.request]);
+
+  if (fetchHook.error) {
+    return <Error error={fetchHook.error} />;
+  }
+  if (fetchHook.loading) {
+    return <Loading />;
+  }
+  if (fetchHook.data) {
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {fetchHook.data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  } else {
+    return null;
+  }
+};
+
+export default FeedPhotos;
