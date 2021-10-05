564291d5a0db40a8e4abe318505a2ffa8916e299
Gabriel Souza <gabrielss_@outlook.com>
2021-10-05 16:59:06 -0300

    estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,34 @@
 import React from "react";
 import Head from "../Helper/Head";
+import useFetch from "../../Hooks/useFetch";
+import { STATS_GET } from "../../api";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+import UserStatsGraphs from "./UserStatsGraphs";
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      UserStats
-    </div>
-  );
+  const fetchHook = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await fetchHook.request(url, options);
+    }
+    getData();
+  }, [fetchHook.request]);
+
+  if (fetchHook.loading) return <Loading />;
+  if (fetchHook.error) return <Error error={fetchHook.error} />;
+  if (fetchHook.data) {
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={fetchHook.data} />
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default UserStats;
